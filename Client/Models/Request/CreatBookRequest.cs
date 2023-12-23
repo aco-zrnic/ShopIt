6ce732845f9fdd6a839808b81a6f226654c4b18c
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-12-23 18:44:22 +0100

    add client to the server

--- Client/Models/Request/CreatBookRequest.cs
+++ Client/Models/Request/CreatBookRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -14,6 +15,11 @@ namespace Client.Models.Request
         public string Title { get; set; }
         public string Description { get; set; }
         public string MainActors { get; set; }
+        [Required]
+        public DateTimeOffset ReleaseData { get; set; }
+        [Required]
+        [RegularExpression(RegexPattern.Base64String, ErrorMessage = "The provided string is not valid base64 string")]
+        public string CoverImage { get; set; }
         public bool IsComic { get; set; }
     }
 }
---
+++ Client/RegexPattern.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class RegexPattern
+    {
+        public const string Base64String = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$";
+    }
+}
