55e92d5d7f9a5ba2bf663792a7b5f1dc1dfa0a90
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-12-23 18:44:37 +0100

    add api mapping profile

---
+++ Server/AutoMapper/ApiMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Client.Models.Request;
+using Server.Models.Request;
+
+namespace Server.AutoMapper
+{
+    public class ApiMappingProfile : Profile
+    {
+        public ApiMappingProfile()
+        {
+            CreateRequestMappings();
+            CreateResponseMappings();
+        }
+        private void CreateRequestMappings()
+        {
+            CreateMap<CreatBookRequest, CreateBook>();
+        }
+        private void CreateResponseMappings()
+        {
+
+        }
+    }
+}
