c052b90955dfef4923ce942b3183b49e1876ae76
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-12-23 16:38:05 +0100

    add client project to server

---
+++ Client/Models/Request/CreatBookRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models.Request
+{
+    public class CreatBookRequest
+    {
+
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string MainActors { get; set; }
+        public bool IsComic { get; set; }
+    }
+}
