d8e5e1e9d8a52dcf6a0a4b8c784b24a1b9f70ab4
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-12-23 19:22:48 +0100

    Add aws gateway options for configuration

---
+++ Server/Options/AwsS3Options.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Options
+{
+    public class AwsS3Options
+    {
+        public const string Gateway = "AwsConfiguration";
+        [Required]
+        public string BucketName { get; set; }
+        [Required]
+        public string AWSAccessKey { get; set; }
+        [Required]
+        public string AWSSecretKey { get; set; }
+    }
+}
