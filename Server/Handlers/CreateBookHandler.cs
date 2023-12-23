cff18d4fa643ee3e9d0787636511c6bb3e1d1972
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-12-23 19:24:30 +0100

    add template for create book handler

---
+++ Server/Handlers/CreateBookHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Server.Models.Request;
+using Server.Models.Response;
+
+namespace Server.Handlers
+{
+    public class CreateBookHandler : IRequestHandler<CreateBook, CreatedBook>
+    {
+        public Task<CreatedBook> Handle(CreateBook request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
