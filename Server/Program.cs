47b86387f2c2070ec924aebbb61e6106f99f1320
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-12-24 13:45:41 +0100

    update program.cs register 2 new moduls

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,41 @@
+using Autofac;
+using Autofac.Core;
+using Autofac.Extensions.DependencyInjection;
+using MediatR.Extensions.Autofac.DependencyInjection;
+using MediatR.Extensions.Autofac.DependencyInjection.Builder;
+using MediatR.Pipeline;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using Server.Behavior;
+using Server.Modules;
+using Server.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host
+    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+     .ConfigureContainer<ContainerBuilder>(builder =>
+     {
+         var configuration = MediatRConfigurationBuilder
+           .Create(typeof(Program).Assembly)
+           .WithAllOpenGenericHandlerTypesRegistered()
+           .Build();
+
+         // this will add all your Request- and Notificationhandler
+         // that are located in the same project as your program-class
+
+         builder.RegisterMediatR(configuration);
+
+         
+         builder.RegisterModule<BehaviorModule>();
+         builder.RegisterModule<ShopItModule>();
+     });
 
 // Add services to the container.
-
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddOptions<AwsS3Options>()
+    .BindConfiguration(AwsS3Options.ConfigSection)
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
