9dcfc54d79f908d90d728a240d5bdfe42523e0e3
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-12-24 17:37:52 +0100

    add module for registering services

---
+++ Server/Modules/ShopItModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Server.Services;
+
+namespace Server.Modules
+{
+    public class ShopItModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AwsS3Service>().As<IAwsS3Service>().InstancePerDependency();
+        }
+    }
+}
