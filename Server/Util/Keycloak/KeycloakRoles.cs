f35c490b34eca46b38db2b92416b9dd090e91824
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-12-25 11:20:03 +0100

    add keycloack roles

---
+++ Server/Util/Keycloak/KeycloakRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Util.Keycloak
+{
+    public class KeycloakRoles
+    {
+        public const string Admin = "admin"; 
+        public const string User = "user"; // user/guest
+        public const string All = "admin,user";
+    }
+}
