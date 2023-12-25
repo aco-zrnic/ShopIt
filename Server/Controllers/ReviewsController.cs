2c23d417b23fe039b895de70c1351d02fe32acad
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-12-25 18:18:54 +0100

    add review controller

---
+++ Server/Controllers/ReviewsController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Client.Models.Request;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Server.Behavior;
+using Server.Models.Request;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ServiceFilter(typeof(LoggingActionFilterBehavior))]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public ReviewsController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ReviewsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ReviewsController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] AddScoreRequest request)
+        {
+            var response = await _mediator.Send(_mapper.Map<AddScore>(request));
+            return StatusCode(StatusCodes.Status201Created, response);
+        }
+
+        // PUT api/<ReviewsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ReviewsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
